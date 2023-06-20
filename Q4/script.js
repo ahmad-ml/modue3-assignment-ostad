90563aa9a8719fc6c48433974a804c9ab1bc25f9
ahmad-ml <ahmad@niftyitsolution.com>
2023-06-21 00:10:06 +0600

    Q4 ans
    This code creates a button on the webpage. When the button is clicked, it calls the openNewWindow function defined in the script.js file. The openNewWindow function uses the window.open method to open a new window with the specified URL ("https://niftyitsolution.com") and dimensions (800x600).

---
+++ Q4/script.js
@@ -0,0 +1,8 @@
+function openNewWindow() {
+    var url = "https://niftyitsolution.com";
+    var width = 800;
+    var height = 600;
+    var windowFeatures = "width=" + width + ",height=" + height;
+
+    window.open(url, "", windowFeatures);
+}
